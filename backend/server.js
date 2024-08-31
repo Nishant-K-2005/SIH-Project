e5198f63ec97b026bcdf35fe0269d49a0f970062
Rahul Singh <rahulsingh.dev.36@gmail.com>
2024-08-31 23:30:22 +0530

    initial env setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+mongoose.connect(process.env.URI)
+    .then(() => {
+        console.log("Connected Succesfully");
+        app.listen(process.env.PORT || 8000, (err) => { 
+            if (err) throw err;
+            console.log("Server is running at", process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB", error);
+        process.exit(1);
+    }
+    );
+
+app.get("/", (req, res) => {
+    res.send("api running test");
+});
+
