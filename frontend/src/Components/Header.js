5841ba5adc4440bbe773f042bb2418f3a0a19e7d
Nishant <nishantkarena10@gmail.com>
2024-09-01 15:09:30 +0530

    Added landing page

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import AICTElogo from './AICTE-logo.png'
+import AICTElogo from '../images/AICTE-logo.png'
 export default function Header() {
   return (
-    <ul className='list-none flex justify-between items-center px-5 h-14 bg-yellow-300'>
-        <li className='font-semibold text-xl'>
+    <ul className='list-none flex justify-between items-center px-5 h-fit bg-yellow-300'>
+        <li className='font-semibold text-xl w-2/5 md:w-1/2'>
             Special Scholarship Scheme for J&K and Ladakh (PM-USPY)
         </li>
-        <li className='flex items-center text-xs'>
+        <li className='flex items-center text-xs w-2/5 md:w-1/2 md:justify-end'>
             <img className='h-14' src={AICTElogo}/>All India Council for Technical Education
         </li>
     </ul>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'home-bg':"ur"
+      }
+    },
   },
   plugins: [],
 }
